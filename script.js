7cf7a84bebc27b25e19b7d06295507d1d18efc76
mirellasfs <130072970+mirellasfs@users.noreply.github.com>
2023-05-05 09:13:23 -0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<script>
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +25,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
-</script>
